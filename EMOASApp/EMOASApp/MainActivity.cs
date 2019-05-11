2d897e2043013fed58104793b94c11aa4575ba49
Chenxu <sugarmanyin2017@outlook.com>
2019-05-11 09:19:40 +0800

    create xamarin.android

---
+++ EMOASApp/EMOASApp/MainActivity.cs
@@ -0,0 +1,21 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+
+namespace EMOASApp
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_login);
+            string url = "http://192.168.1.117:5000/message/test";
+        }
+    }
+}
---
+++ EMOASApp/EMOASApp/Utils/HttpUtil.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Newtonsoft.Json;
+
+namespace EMOASApp.Utils
+{
+    class HttpUtil
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="args"></param>
+        public async static Task<string> GetRouteData(string url, object args)
+        {
+            using (var client = new HttpClient())
+            {
+                try
+                {
+                    //序列化
+                    var str = JsonConvert.SerializeObject(args);
+
+                    HttpContent content = new StringContent(str);
+                    content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
+                    HttpResponseMessage response = await client.PostAsync(url, content);//改成自己的
+
+                    response.EnsureSuccessStatusCode();//用来抛异常的
+                    return await response.Content.ReadAsStringAsync();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("\nException Caught!");
+                    Console.WriteLine("Message :{0} ", e.Message);
+                    return "error";
+                }
+            }
+        }
+    }
+}
